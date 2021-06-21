00e6792a5acdd66520779db650f60d2201c0fc5e
miguel <miguelbarreraa05@gmail.com>
2021-06-21 20:32:11 +0000

    Avanzando en registrar los bancos

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
 <?php
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', dirname(__FILE__) . '/log.txt');
 error_reporting(E_ALL);
 
-require('conexion.php');
 require('vendor/autoload.php');
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -39,7 +30,6 @@ try {
 	echo "<select name='prueba'>";
 	foreach ($items as $clave => $valor) {
 	 	$final = strval($items[$clave]->bankName);
-		#print_r($items[$clave]->bankName);
 		echo "<option value='$final'>$final</options>";
 	}
 	echo "</select>";
@@ -47,10 +37,4 @@ try {
 	echo "Error $e";
 }
 
-$stmt = Conexion::conectar()->prepare("SELECT * FROM bancos");
-$stmt->execute();
-echo print_r($stmt->fetch());
 ?>
-	
-</body>
-</html>
