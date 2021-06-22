134fa5f9b498090d2998e354a48cad7b78cf5f20
Miguel <miguelbarreraa05@gmail.com>
2021-06-21 23:07:31 -0500

    tarea diaria lista

--- bancos.php
+++ bancos.php
@@ -1,14 +1,13 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-ini_set('error_log', dirname(__FILE__) . '/log.txt');
-error_reporting(E_ALL);
+// Para programar la tarea diariamente usamos 'cron' con el comando 
+// 00 18 * * * /usr/bin/php /opt/lamp/htdocs/probandoPHP/bancos.php
 
 require('vendor/autoload.php');
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+//Objeto atuh para conectar con placetopay
 $url = $_ENV['URL'];
 $login = $_ENV['TOKEN'];
 $secretKey = $_ENV['SECRETKEY'];
@@ -23,32 +22,33 @@ $auth = [
 ];
 
 try {
+
+	// Conexion base de datos
 	$link = new PDO("mysql:host=localhost;dbname=pruebaPSE",
 		"root",
 		"");
 	$link->exec("set names utf8");
+
+	// Conexion placetopay
 	$client = new SoapClient($url);
 	$result = $client->getBankList($auth);
 	$items = $result->getBankListResult->item;
-	$stmt = $link->query("SELECT codigo, nombre FROM bancos");
-	$currents = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	print_r($currents[0]);
-	foreach ($items as $clave => $valor) {
-	 	$final = strval($items[$clave]->bankName);
-		echo $final."<br>";
-	}
 
-	#foreach ($items as $clave => $valor) {
-	#	$stmt = $link->prepare("INSERT INTO bancos (codigo, nombre) VALUES (:codigo, :nombre)");
-	# 	$nombreBanco = strval($items[$clave]->bankName);
-	# 	$codigoBanco = strval($items[$clave]->bankCode);
-	#	$stmt->bindParam(":codigo", $codigoBanco, PDO::PARAM_INT);
-	#	$stmt->bindParam(":nombre", $nombreBanco, PDO::PARAM_STR);
-	#	$stmt->execute();
-	#	echo "<br>";
-	#}
+	foreach ($items as $clave => $valor) {
+		try {
+			// Si el elemento no esta en la base de datos
+			$stmt = $link->prepare("INSERT INTO bancos (codigo, nombre) VALUES (:codigo, :nombre)");
+			$nombreBanco = strval($valor->bankName);
+			$codigoBanco = strval($valor->bankCode);
+			$stmt->bindParam(":codigo", $codigoBanco, PDO::PARAM_INT);
+			$stmt->bindParam(":nombre", $nombreBanco, PDO::PARAM_STR);
+			$stmt->execute();
+		} catch (Exception $e) {
+			// Si el elemento ya esta en la base de datos
+			print_r($valor);
+			echo $e->getMessage()."<br>";
+		}
+	}
 } catch (Exception $e) {
-	echo '<div class="alert alert-info" role="alert" style ="font-size:12pt;">
-		<strong>Falló la conexión: !</strong> '.$e->getMessage().'
-		</div>';
+	echo $e->getMessage();
 }
